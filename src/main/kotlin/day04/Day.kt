a67715e3824efcdb534479b2e65777ee6c30f5f1
Scott King <scottmk@amazon.com>
2023-12-03 22:28:11 -0800

    Add intersection to helper

--- src/main/kotlin/day04/Day.kt
+++ src/main/kotlin/day04/Day.kt
@@ -4,20 +4,16 @@ import parseInput
 import java.util.Scanner
 
 class Day(val input: Scanner) {
-    fun starOne(): Int = getRounds(input).sumOf { (card, winningNums) ->
-        card.toSet().intersect(winningNums.toSet()).fold(0) { result, _ ->
-            if (result == 0) 1 else result * 2
-        }.toInt()
+    fun starOne(): Int = getRounds(input).sumOf {
+        it.fold(0) { result, _ -> if (result == 0) 1 else result * 2 }.toInt()
     }
 
     fun starTwo(): Int {
         val rounds = getRounds(input).toList()
         val allCards = IntArray(rounds.size) { 1 }
 
-        rounds.forEachIndexed { index, (card, winningNums) ->
-            card.toSet().intersect(winningNums.toSet()).forEachIndexed { i, _ ->
-                allCards[index + i + 1] += allCards[index]
-            }
+        rounds.forEachIndexed { index, intersection ->
+            intersection.forEachIndexed { i, _ -> allCards[index + i + 1] += allCards[index] }
         }
 
         return allCards.sum()
@@ -26,9 +22,7 @@ class Day(val input: Scanner) {
     private fun getRounds(input: Scanner) = parseInput(input)
         .map { it.split("|") }
         .map { (left, right) ->
-            listOf(
-                left.split(":")[1].split(" ").filter { it.isNotEmpty() },
-                right.split(" ").filter { it.isNotEmpty() }
-            )
+            left.split(":")[1].split(" ").filter { it.isNotEmpty() }.toSet()
+                .intersect(right.split(" ").filter { it.isNotEmpty() }.toSet())
         }
 }
