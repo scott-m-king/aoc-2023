e5a48380d426d25025366f650302899cb27f2667
Scott King <scottmk@amazon.com>
2023-12-05 22:51:08 -0800

    Last one

--- src/main/kotlin/day06/Day.kt
+++ src/main/kotlin/day06/Day.kt
@@ -6,11 +6,10 @@ import java.util.Scanner
 class Day(val input: Scanner) {
     fun starOne(): Int = parseInput(input).map { it.split(" ").filter(String::isNotEmpty).drop(1) }
         .toList().let { (times, distances) ->
-            times.foldIndexed(1) { i, acc, _ ->
+            distances.foldIndexed(1) { i, acc, distance ->
                 val time = times[i].toInt()
-                val distance = distances[i].toInt()
-                acc * (0..time).foldIndexed(0) { iter, counter, _ ->
-                    if (iter * (time - iter) > distance) counter + 1 else counter
+                acc * (0..time).reduce { counter, iter ->
+                    if (iter * (time - iter) > distance.toInt()) counter + 1 else counter
                 }
             }
         }
@@ -18,6 +17,6 @@ class Day(val input: Scanner) {
     fun starTwo(): Long = parseInput(input).map { it ->
         Regex(" [0-9]+").findAll(it).map { it.value.trim() }.joinToString("")
     }.map(String::toLong).toList().let { (time, distance) ->
-        (0..time).foldIndexed(0.toLong()) { iter, acc, _ -> if (iter * (time - iter) > distance) acc + 1 else acc }
+        (0.toLong()..time).reduce { acc, iter -> if (iter * (time - iter) > distance) acc + 1 else acc }
     }
 }
